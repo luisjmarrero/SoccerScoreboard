6e94b3442fdaf935aaace78224920d1ea59b68a2
luisjosemarrero <luis124ma@gmail.com>
2017-04-06 18:31:22 -0400

    (add) - some frontend work! #hateit

--- src/main/java/tecnicas/software/view/ViewController.java
+++ src/main/java/tecnicas/software/view/ViewController.java
@@ -12,8 +12,23 @@ import java.util.Date;
 @Controller
 public class ViewController {
 
-    @RequestMapping("/score")
+    @RequestMapping(value={"", "/", "home"})
     public String index(Model model){
+        return "index";
+    }
+
+    @RequestMapping("/history")
+    public String history(Model model){
+        return "history";
+    }
+
+    @RequestMapping("/admin")
+    public String admin(Model model){
+        return "admin";
+    }
+
+    @RequestMapping("/score")
+    public String score(Model model){
         model.addAttribute("title", "Soccer ScoreBoard");
         model.addAttribute("minute", 85);
         model.addAttribute("datetime", new Date());
