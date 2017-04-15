196c65dbe71faea8d4270b6c99d32d66ec75ab90
luisjosemarrero <luis124ma@gmail.com>
2017-04-15 02:30:48 -0400

    swagger filter classes

--- src/main/java/tecnicas/software/SoccerscoreboardApplication.java
+++ src/main/java/tecnicas/software/SoccerscoreboardApplication.java
@@ -11,12 +11,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -30,8 +30,15 @@ public class SoccerscoreboardApplication {
 	public Docket swaggerSpringMvcPlugin() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.useDefaultResponseMessages(false)
+				.apiInfo(apiInfo())
 				.select()
-				.paths(Predicates.not(PathSelectors.regex("/error.*")))
+//				.paths(Predicates.not(regex("/error.*")))
+				.paths(Predicates.or(
+						regex("/games.*"),
+						regex("/players.*"),
+						regex("/teams.*"),
+						regex("/plays.*")
+				))
 				.build();
 	}
 
@@ -49,11 +56,11 @@ public class SoccerscoreboardApplication {
 
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
-				.title("Springfox petstore API")
-				.description("description")
-				.contact(new Contact("name", "url", "email"))
+				.title("FootBoard API")
+				.description("REST API Docummentation for FootBoard App")
 				.license("Apache License Version 2.0")
-				.version("2.0")
+				.version("1.0")
 				.build();
 	}
+
 }
